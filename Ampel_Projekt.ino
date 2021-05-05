acbef01077003ccc2c9177b8f393e06c7530927e
Zoiiid <marvin.kreienbuehl@leunet.ch>
2021-05-05 15:43:32 +0200

    Main

---
+++ Ampel_Projekt.ino
@@ -0,0 +1,30 @@
+/* Ampel Projekt
+ * Copyright Code: Marvin Kreienbühl, Fabrice Maurer, Marlo Cadonau
+ */
+
+
+void setup() {
+  int Ampel_Kreuz_NS_rot =
+  int Ampel_Kreuz_NS_gelb =
+  int Ampel_Kreuz_NS_grün = 
+  int Ampel_Kreuz_OW_rot =
+  int Ampel_Kreuz_OW_gelb =
+  int Ampel_Kreuz_OW_grün =
+  int Ampel_Fussgaenger_NS1_rot =
+  int Ampel_Fussgaenger_NS1_gelb =
+  int Ampel_Fussgaenger_NS1_grün =
+  int Ampel_Fussgaenger_NS2_rot =
+  int Ampel_Fussgaenger_NS2_gelb =
+  int Ampel_Fussgaenger_NS2_grün =
+  int Ampel_Fussgaenger_OW1_rot =
+  int Ampel_Fussgaenger_OW1_gelb =
+  int Ampel_Fussgaenger_OW1_grün =
+  int Ampel_Fussgaenger_OW2_rot =
+  int Ampel_Fussgaenger_OW2_gelb =
+  int Ampel_Fussgaenger_OW2_grün =
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
