55cc41e732e5a866b2276352fbf6d0380645ca31
Zoiiid <marvin.kreienbuehl@leunet.ch>
2021-05-12 15:27:08 +0200

    minor changes

--- Ampel_Projekt.ino
+++ Ampel_Projekt.ino
@@ -4,24 +4,24 @@
 
 
 void setup() {
-  int Knopf =
-  int Ampel_Kreuz_NS_rot = 
-  int Ampel_Kreuz_NS_gelb =
-  int Ampel_Kreuz_NS_grün = 
-  int Ampel_Kreuz_OW_rot =
-  int Ampel_Kreuz_OW_gelb =
-  int Ampel_Kreuz_OW_grün =
-  int Ampel_Fussgaenger_NS_rot =
-  int Ampel_Fussgaenger_NS_gelb =
-  int Ampel_Fussgaenger_NS_grün =
-  int Ampel_Fussgaenger_OW_rot =
-  int Ampel_Fussgaenger_OW_gelb =
-  int Ampel_Fussgaenger_OW_grün =
-  pinmode(Knopf, INPUT)
+  int Knopf = null;
+  int Ampel_Kreuz_NS_rot = null;
+  int Ampel_Kreuz_NS_gelb = null;
+  int Ampel_Kreuz_NS_grün = null;
+  int Ampel_Kreuz_OW_rot = null;
+  int Ampel_Kreuz_OW_gelb = null;
+  int Ampel_Kreuz_OW_grün = null;
+  int Ampel_Fussgaenger_NS_rot = null;
+  int Ampel_Fussgaenger_NS_gelb = null;
+  int Ampel_Fussgaenger_NS_grün = null;
+  int Ampel_Fussgaenger_OW_rot = null;
+  int Ampel_Fussgaenger_OW_gelb = null;
+  int Ampel_Fussgaenger_OW_grün = null;
+  pinmode(Knopf, INPUT);
   pinmode(Ampel_Kreuz_NS_rot, OUTPUT);
   pinmode(Ampel_Kreuz_NS_gelb, OUTPUT);
   pinmode(Ampel_Kreuz_NS_grün, OUTPUT);
-  pinmode(Ampel_Kreuz_OW_rot, OUTPUT );
+  pinmode(Ampel_Kreuz_OW_rot, OUTPUT);
   pinmode(Ampel_Kreuz_OW_gelb, OUTPUT);
   pinmode(Ampel_Kreuz_OW_grün, OUTPUT);
   pinmode(Ampel_Fussgaenger_NS_rot, OUTPUT);
@@ -33,6 +33,6 @@ void setup() {
 }
 
 void loop() {
-
+  
 
 }
