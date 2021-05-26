6f7433d593cb3289afea2328b32e07fbdfe3e9de
Zoiiid <marvin.kreienbuehl@leunet.ch>
2021-05-26 16:02:10 +0200

    Final Changes

--- Ampel_Projekt.ino
+++ Ampel_Projekt.ino
@@ -1,22 +1,21 @@
 /* Ampel Projekt 
  * Copyright Code: Marvin Kreienbühl, Fabrice Maurer, Marlo Cadonau
  */
-  int Knopf = 1;
+  int Knopf = 13;
   int Ampel_Kreuz_NS_rot = 2;
   int Ampel_Kreuz_NS_gelb = 3;
   int Ampel_Kreuz_NS_gruen = 4;
-  int Ampel_Kreuz_OW_rot = 5;
+  int Ampel_Kreuz_OW_rot = 7;
   int Ampel_Kreuz_OW_gelb = 6;
-  int Ampel_Kreuz_OW_gruen = 7;
-  int Ampel_Fussgaenger_NS_rot = 8;
-  int Ampel_Fussgaenger_NS_gelb = 9;
-  int Ampel_Fussgaenger_NS_gruen = 10;
+  int Ampel_Kreuz_OW_gruen = 5;
+  int Ampel_Fussgaenger_NS_rot = 9;
+  int Ampel_Fussgaenger_NS_gruen = 8;
   int Ampel_Fussgaenger_OW_rot = 11;
-  int Ampel_Fussgaenger_OW_gelb = 12;
-  int Ampel_Fussgaenger_OW_gruen = 13;
+  int Ampel_Fussgaenger_OW_gruen = 10;
   int Knopfvalue = 0;
  //Initialisiert alle Variablen, welche wir für dieses Projekt brauchen. OW = Ost-West, NS= Nord-Süd
 void setup() {
+  Serial.begin(9600);
   pinMode(Knopf, INPUT);
   pinMode(Ampel_Kreuz_NS_rot, OUTPUT);
   pinMode(Ampel_Kreuz_NS_gelb, OUTPUT);
@@ -25,17 +24,16 @@ void setup() {
   pinMode(Ampel_Kreuz_OW_gelb, OUTPUT);
   pinMode(Ampel_Kreuz_OW_gruen, OUTPUT);
   pinMode(Ampel_Fussgaenger_NS_rot, OUTPUT);
-  pinMode(Ampel_Fussgaenger_NS_gelb, OUTPUT);
   pinMode(Ampel_Fussgaenger_NS_gruen, OUTPUT);
   pinMode(Ampel_Fussgaenger_OW_rot, OUTPUT);
-  pinMode(Ampel_Fussgaenger_OW_gelb, OUTPUT);
   pinMode(Ampel_Fussgaenger_OW_gruen, OUTPUT);
   //Setzt alle LEDs auf OUTPUT und den Knopf auf INPUT
 }
 
 void loop() {
   Knopfvalue = digitalRead(Knopf);
-  if (Knopfvalue == HIGH) {
+  Serial.println(Knopfvalue);
+  if (Knopfvalue == 0) {
     digitalWrite(Ampel_Kreuz_NS_rot, HIGH);
     digitalWrite(Ampel_Kreuz_OW_rot, HIGH);
     digitalWrite(Ampel_Fussgaenger_OW_rot, HIGH);
@@ -43,7 +41,7 @@ void loop() {
     delay(1000);
     digitalWrite(Ampel_Kreuz_NS_gelb, HIGH);
     delay(1000);
-    digitalWrite(Ampel_Fussgaenger_NS_rot, LOW);
+    digitalWrite(Ampel_Kreuz_NS_rot, LOW);
     delay(500);
     digitalWrite(Ampel_Kreuz_NS_gelb, LOW);
     digitalWrite(Ampel_Kreuz_NS_gruen, HIGH);
@@ -56,16 +54,18 @@ void loop() {
     digitalWrite(Ampel_Kreuz_NS_rot, HIGH);
     digitalWrite(Ampel_Fussgaenger_OW_rot, HIGH);
     //Alle Aus
-    delay(1500);
+    delay(1000);
     digitalWrite(Ampel_Kreuz_OW_gelb, HIGH);
     delay(1000);
-    digitalWrite(Ampel_Fussgaenger_OW_rot, LOW);
+    digitalWrite(Ampel_Kreuz_OW_rot, LOW);
     delay(500);
     digitalWrite(Ampel_Kreuz_OW_gelb, LOW);
-    digitalWrite(Ampel_Fussgaenger_OW_gruen, HIGH);
+    digitalWrite(Ampel_Kreuz_OW_gruen, HIGH);
     digitalWrite(Ampel_Fussgaenger_NS_rot, LOW);
     digitalWrite(Ampel_Fussgaenger_NS_gruen, HIGH);
     delay(10000);
+    digitalWrite(Ampel_Fussgaenger_NS_gruen, LOW);
+    digitalWrite(Ampel_Kreuz_OW_gruen, LOW);
   }
 
   else {
